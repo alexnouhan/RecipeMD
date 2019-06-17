22ed5b4a7faf8e3005060cc9f8cf62429e5e0b05
alexnouhan <alexnouhan@gmail.com>
2019-06-17 11:44:58 -0400

    added restriction table to db

--- userprofile.sql
+++ userprofile.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `recipe_md` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `recipe_md`;
 -- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
 -- Host: localhost    Database: recipe_md
@@ -14,6 +16,29 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_restrictions`
+--
+
+DROP TABLE IF EXISTS `user_restrictions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_restrictions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `restriction` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_restrictions`
+--
+
+LOCK TABLES `user_restrictions` WRITE;
+/*!40000 ALTER TABLE `user_restrictions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_restrictions` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +49,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-17 11:40:10
+-- Dump completed on 2019-06-17 11:44:42
